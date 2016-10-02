eccdc323794a737f1052f885fd86c59b06a27bfa
VasilevsVV <morfey.rulit@gmail.com>
2016-10-02 16:29:01 +0300

    entities created.

---
+++ Db_lab1/entity.py
@@ -0,0 +1,17 @@
+class country:
+
+    def __init__(self, name, mainl, currency):
+        self.name = name
+        self.mainl = mainl
+        self.currency = currency
+
+    # def make_string(self):
+    #     return self.name + ':' + self.mainl + ':' + str(self.currency)
+
+
+class city:
+
+    def __init__(self, name, population, area):
+        self.name = name
+        self.population = population
+        self.area = area
---
+++ Db_lab1/test.py
@@ -0,0 +1,2 @@
+from entity import country
+from entity import city
